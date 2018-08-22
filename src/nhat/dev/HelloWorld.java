6c1cdebe50ef18ed24e637fbf4faceabd75e92e0
nhat <nhat.nguyenminh@students.hueic.edu.vn>
2018-08-22 17:02:18 +0700

    Finish phuongtrinhbachai

--- src/nhat/dev/HelloWorld.java
+++ src/nhat/dev/HelloWorld.java
@@ -2,11 +2,25 @@ package nhat.dev;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		for(int i=0;i<=9;i++) {
-			for(int j=0;j<=9;j++) {
-				System.out.print(j + " * " + i + " = " + i * j + "\t");
-			}
-			System.out.println();
-		}
+		System.out.println("Hello world");
+		  double a = 10;
+		  double b = 8;
+		  double c = 3;
+		  double delta;
+		  double x1, x2;
+		  delta = (b*b) - (4*a*c);
+		  if (delta < 0) {
+			   System.out.println("Phuong Trinh Vo Nghiem");
+		  }
+		  if (delta > 0) {
+			  x1 = (-b+Math.sqrt(delta)) / (2*a);
+			  x2 = (-b-Math.sqrt(delta)) / (2*a);
+			  System.out.println("x1 = " + x1);
+			  System.out.println("x2 = " + x2);
+		  }
+		  if (delta == 0) {
+			  x1 = -b / (2*a);
+			  System.out.println("Phuong Trinh Co Nghiem Kep x = " + x1);
+		  }
 	}
 }
